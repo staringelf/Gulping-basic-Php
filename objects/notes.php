c66f8227ccce9e01b4229e2da57d3c15271bb30d
Manish Sajwan <msoffice37@gmail.com>
2021-03-27 02:22:05 -0700

    Update notes.php

--- objects/notes.php
+++ objects/notes.php
@@ -1,5 +1,31 @@
-//Access OPerators 
+<pre>
+  <?php
+  //Access OPerators 
 
-//' -> ' for attributes and methods, instance
+  //' -> ' for attributes and methods, instance
 
-//' :: ' for constants and statics 
+  //' :: ' for constants and statics 
+
+  /*-- Object Life Cycle */
+  //Objects are created, used and discarded
+
+  class PA {  //PartyAnimal
+    function __construct() {
+      echo("Constructed\n");
+    }
+    function something() {
+      echo("Something\n");
+    }
+    function __destruct() {
+      echo("Destructed\n");
+    }
+  }
+    
+  $x = new PA();
+  $y = new PA();
+  $x->something();
+  $y->something();
+  echo("The-End ??\n");
+
+  ?>
+</pre>
